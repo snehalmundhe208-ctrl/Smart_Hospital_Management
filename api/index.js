0ee4b1b3e62a156fbe249f77ada87da27d668665
Snehal <snehalmundhe208@gmail.com>
2026-07-19 14:08:57 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -1,3 +1,6 @@
+console.log("DATABASE_URL CHECK:", process.env.DATABASE_URL);
+console.log("PGHOST CHECK:", process.env.PGHOST);
+console.log("PGDATABASE CHECK:", process.env.PGDATABASE);
 const app = require('../backend/index.js');
 
 module.exports = app;
